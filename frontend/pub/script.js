7396616c738f76505b55253854c406edf8eb4750
GergelyKiss <gergely.kiss.hun@gmail.com>
2022-03-26 22:28:03 +0100

    more data

--- frontend/pub/script.js
+++ frontend/pub/script.js
@@ -1,17 +1,26 @@
 // Components
-
-const containerHTML = (recBeerHTML) => {
+const navbarHTML = (recNavAnchorsHTML) => {
     return `
-    <section class="container">
-			<h1>Beers Page</h1>
-			<div class="beer-cards">
-				${recBeerHTML}
-			</div>
-		</section>
+    <nav class="navbar">
+        <ul class="nav-list">
+            ${recNavAnchorsHTML}
+        </ul>
+    </nav>
+    `;
+};
+
+const containerHTML = (sectionName, recBeerHTML) => {
+    return `
+    <section id="${sectionName}" class="container">
+        <h1>${sectionName}</h1>
+        <div class="beer-cards">
+            ${recBeerHTML}
+        </div>
+    </section>
     `
 };
 
-const BeerHTML = (beerList) => {
+const cardHTML = (beerList) => {
     return Array.from(beerList).map(beer => {
         return `
         <div class="beer-card">
@@ -23,11 +32,51 @@ const BeerHTML = (beerList) => {
     }).join('');
 };
 
+const navAnchorsHTML = () => {
+    const anchorList = [
+        {
+            link: '#Beers',
+            title: 'Beers',
+            iconClass: 'fas fa-beer-mug-empty'
+        },
+        {
+            link: '#Wine',
+            title: 'Wine',
+            iconClass: 'fas fa-wine-glass'
+        },
+        {
+            link: '#Spirits',
+            title: 'Spirits',
+            iconClass: 'fas fa-whiskey-glass'
+        },
+        {
+            link: '#Cocktails',
+            title: 'Cocktails',
+            iconClass: 'fas fa-martini-glass'
+        },
+        {
+            link: '#Quotes',
+            title: 'Quotes',
+            iconClass: 'fas fa-crown'
+        }
+
+    ];
+
+    return anchorList.map(a => {
+        return `
+        <a href="${a.link}">
+            <span>${a.title}</span>
+            <i class="${a.iconClass}"></i>
+        </a>
+        `
+    }).join('');
+};
+
 
 
 //FETCH
 
-const getBeers = async () => {
+const getAllData = async () => {
     const request = await fetch('./pub/data.json');
     const result = await request.json();
     return result;
@@ -40,11 +89,12 @@ const init = async () => {
     const root = document.getElementById('root');
     
     //Get Data
-    const beers = await getBeers();
-    console.log(beers);
+    const allData = await getAllData();
+    console.log(allData);
 
     //Create HTML
-    root.insertAdjacentHTML('beforeend', containerHTML(BeerHTML(beers.cards)));
+    root.insertAdjacentHTML('beforeend', navbarHTML(navAnchorsHTML()));
+    root.insertAdjacentHTML('beforeend', allData.map(section => containerHTML(section.id, cardHTML(section.cards))).join(''));
 
 
 };
